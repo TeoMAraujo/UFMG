023109395f0ec62fdd926e6b0568d89dc57ddd1c
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-04-09 19:40:43 -0300

    Create ponteiros1504.cpp

---
+++ ponteiros1504.cpp
@@ -0,0 +1,126 @@
+// 0) Crie uma função que receba três variveis inteiras como parâmetro, da seguinte forma:
+//    - A primeira deve ser passada por valor
+//    - A segunda deve ser passada por referência 
+//    - A terceira deve ser passada "por referência" usando apontadores (passagem "por referência" disponível em C)
+//    A função deverá somar 1 ao valor de cada uma das 3 variáveis e retornar.
+
+
+int main(){
+
+    // 1) Declare uma variável do tipo inteiro e preencha com o valor informado na entrada
+    int n
+    cin >> n >> endl    
+    // 2) Declare um ponteiro para inteiros e inicialize com valor nulo
+    int *pointer = nullptr
+    
+    // 3) Declare um vetor de inteiros com tamanho informado na entrada e preencha com dados lidos da entrada
+    int vector[n] = {0}
+    for (int i = 0; i < n; i++){
+        cin >> vector[n]
+    }
+
+    // 4) Imprima o ENDEREÇO da variável declarada em (1)
+    cout << &n 
+
+    // 5) Imprima o VALOR da variável declarada em (1)
+    cout << n 
+
+    // 6) Imprima o ENDEREÇO da variável declarada em (2)
+    cout << &pointer 
+    
+    // 7) Imprima o VALOR da variável declarada em (2)
+    cout << pointer 
+
+    // 8) Imprima o ENDEREÇO da variável declarada em (3)
+    cout << &vector[] 
+
+    // 9) Imprima o ENDEREÇO da primeira posição da variável declarada em (3) 
+    cout << &vector[0] 
+
+    // 10) Imprima o VALOR da primeira posição da variável declarada em (3)
+    cout << &vector[0]  
+
+    // 11) Atribua o ENDEREÇO da variável declarada em (1) à variável declarada em (2)
+    *pointer = &n 
+
+    // 12) Imprima o VALOR da variável declarada em (2)  
+    cout << pointer 
+
+    // 13) Imprima o VALOR guardado no ENDEREÇO apontado por (2)
+    
+
+    // 14) Coloque o VALOR '5' no ENDEREÇO apontado por (2)
+    
+
+    // 15) Imprima o VALOR da variável declarada em (1)
+    
+
+    // 16) Atribua o VALOR da variável (3) à variável declarada em (2)
+    
+
+    // 17) Imprima o VALOR da variável declarada em (2)
+    
+
+    // 18) Imprima o VALOR guardado no ENDEREÇO apontado por (2)
+    
+
+    // 19) Atribua o ENDEREÇO da primeira posição de (3) à variável declarada em (2)
+    
+
+    // 20) Compare o valor variáveis (2) e (3), imprimindo 'S' se forem iguais e 'N' se forem diferentes
+
+
+    // 21) Imprima o VALOR da variável declarada em (2)
+    
+
+    // 22) Imprima o VALOR guardado no ENDEREÇO apontado por (2)
+    
+
+    // 23) Multiplique todos os valores do vetor declarado em (3) por '10', porém manipulando apenas a variável (2)
+    
+
+    // 24) Imprima os elementos de (3) a partir variável do vetor utilizando a notação [] (colchetes)
+    
+
+    // 25) Imprima os elementos de (3) a partir variável do vetor utilizando a notação ponteiro/deslocamento
+    // Ou seja, você NÃO deve efetivamente alterar o valor do ponteiro inicial de (3)
+    
+
+    // 26) Imprima os elementos de (3) utilizando a variável (2) e a notação ponteiro/deslocamento
+    // Ou seja, você NÃO deve efetivamente alterar o valor do ponteiro inicial de (2)
+    
+
+    // 27) Atribua o ENDEREÇO da última posição de (3) à variável declarada em (2)
+    
+
+    // 28) Imprima o VALOR da variável declarada em (2)
+    
+
+    // 29) Imprima o VALOR guardado no ENDEREÇO apontado por (2)
+    
+
+    // 30) Declare um ponteiro para ponteiro e o inicialize com o ENDEREÇO da variável (2)
+    
+
+    // 31) Imprima o VALOR da variável declarada em (30)
+    
+
+    // 32) Imprima o ENDEREÇO da variável declarada em (30)
+    
+
+    // 33) Imprima o VALOR guardado no ENDEREÇO apontado por (30)
+    
+
+    // 34) Imprima o VALOR guardado no ENDEREÇO do ponteiro apontado por (30)
+    
+
+    // 35) Crie 3 variáveis interiras e leia o valor delas da entrada
+
+
+    // 36) Chame a função criada em (0) passando as 3 variáveis criadas em (35) como parâmetro.
+
+    
+    // 37) Imprima o valor das 3 variáveis criadas em (35)
+    
+    return 0;
+}
